87953cffc0138f58f0fa8b04f2f59af6ca567c65
Ashish <officialsandwork@gmail.com>
2024-10-09 15:32:34 +0530

    Socket IO Middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import { ErrorHandler, trycatch } from './error.js'
+import { User } from '../models/userModel.js'
 
 export const isAuthenticated = trycatch((req, res, next) => {
   const token = req.cookies['connect-pro-token']
@@ -39,3 +40,30 @@ export const isAuthenticatedAdmin = trycatch((req, res, next) => {
 
   next()
 })
+
+export const socketAuthenticator = async (err, socket, next) => {
+  try {
+    if (err) return next(err)
+
+    const token = socket.request.cookies['connect-pro-token']
+
+    if (!token) {
+      return next(new ErrorHandler('Please Login to access this resource', 401))
+    }
+
+    const isVerified = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await User.findById(isVerified._id)
+
+    if (!user) {
+      return next(new ErrorHandler('Please Login to access this resource', 401))
+    }
+
+    socket.user = user
+
+    return next()
+  } catch (error) {
+    console.log(error)
+    return next(new ErrorHandler('Please Login to access this resource', 401))
+  }
+}
